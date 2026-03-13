822f763f602699fbd2f4ffc6a8ab512ba2d14895
danieami <danieami@stud.ntnu.no>
2026-03-13 11:42:47 +0100

    feat: add basic Firebase + Firestore setup

--- android/src/main/java/com/assignments/mtnpen/android/AndroidLauncher.java
+++ android/src/main/java/com/assignments/mtnpen/android/AndroidLauncher.java
@@ -1,10 +1,17 @@
 package com.assignments.mtnpen.android;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.assignments.mtnpen.MountainPenguins;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
@@ -14,5 +21,23 @@ public class AndroidLauncher extends AndroidApplication {
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
         configuration.useImmersiveMode = true; // Recommended, but not required.
         initialize(new MountainPenguins(), configuration);
+
+        FirebaseApp app = FirebaseApp.initializeApp(this);
+        Log.d("FIREBASE_TEST", "FirebaseApp = " + app);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("message", "Hello Firestore");
+        data.put("time", System.currentTimeMillis());
+
+        db.collection("testCollection")
+            .add(data)
+            .addOnSuccessListener(documentReference ->
+                Log.d("FIREBASE_TEST", "WRITE OK: " + documentReference.getId()))
+            .addOnFailureListener(e ->
+                Log.e("FIREBASE_TEST", "WRITE FAILED", e));
+
+        initialize(new MountainPenguins(), new AndroidApplicationConfiguration());
     }
 }
