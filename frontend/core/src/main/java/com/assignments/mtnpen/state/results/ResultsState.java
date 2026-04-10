a0eb63c887b7fc5fca1b1b11384b71c98f221293
RyanSims67 <rsim6115@gmail.com>
2026-04-10 09:34:29 +0200

    Added State manager + State templates

---
+++ frontend/core/src/main/java/com/assignments/mtnpen/state/results/ResultsState.java
@@ -0,0 +1,20 @@
+package com.assignments.mtnpen.state.results;
+
+import com.assignments.mtnpen.state.base.BaseState;
+
+public class ResultsState extends BaseState {
+    @Override
+    public void create() {
+        // test
+    }
+
+    @Override
+    public void enter() {
+        // test
+    }
+
+    @Override
+    public void leave() {
+        // test
+    }
+}
